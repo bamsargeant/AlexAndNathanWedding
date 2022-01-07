ea1fe403e59f5c40bd567f461951fcfcacef620f
Brett Sargeant <infotech@edenexchange.com>
2022-01-07 18:22:51 +1100

    Remove unfinished pages - Add google map - Add RSVP

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+
 import { TitleBannerComponent } from './title-banner/title-banner.component';
 import { MapComponent } from './map/map.component';
 import { WeddingDateComponent } from './wedding-date/wedding-date.component';
@@ -8,7 +11,6 @@ import { GalleryItemComponent } from './gallery-item/gallery-item.component';
 import { RsvpFormComponent } from './rsvp-form/rsvp-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
-
 
 
 @NgModule({
@@ -23,7 +25,10 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    GoogleMapsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
   ],
   exports: [
     TitleBannerComponent,
@@ -32,7 +37,7 @@ import { RouterModule } from '@angular/router';
     CarouselComponent,
     GalleryItemComponent,
     RsvpFormComponent,
-    NavbarComponent
+    NavbarComponent,
   ]
 })
 export class ComponentsModule { }
