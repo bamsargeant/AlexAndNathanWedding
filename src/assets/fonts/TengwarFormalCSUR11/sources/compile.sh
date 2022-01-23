14ba5721751ae99b5d5dea153843b8b53a8c1605
Brett Sargeant <infotech@edenexchange.com>
2022-01-23 16:48:55 +1100

    Auto-generated commit [ci skip]

--- src/assets/fonts/TengwarFormalCSUR11/sources/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fontforge -script dansmith2unicode.py
-
-cp TengwarFormalCSUR_dumb.ttf TengwarFormalCSUR_AAT.ttf
-ftxenhancer -m TengwarFormalCSUR.mif TengwarFormalCSUR_AAT.ttf
-
-grcompiler TengwarFormalCSUR.gdl TengwarFormalCSUR_AAT.ttf TengwarFormalCSUR.ttf
